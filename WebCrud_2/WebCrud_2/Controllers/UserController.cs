a723e2c4091baf604f06976d24859ab8768a5a80
Francisco Miguel Suero Nin <franciscosueronin@gmail.com>
2017-01-10 22:33:34 -0400

    moded added, database added, controler 20% added

---
+++ WebCrud_2/WebCrud_2/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCrud_2.Models;
+
+namespace WebCrud_2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult LogIn(string username, string password)
+        {
+            using (UserEntities userModel = new UserEntities())
+            {
+                var objectUser = from user in userModel.pusers
+                                 where user.username == username && user.password == password
+                                 select user;
+
+                return Json((objectUser), JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
+    }
+}
